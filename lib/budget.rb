f5f4f81153e637421214d05e0adfd2e271513bc5
Cody Brubaker and Jim Padilla <cody@>
2016-12-19 11:32:07 -0800

    add validation specs to budget class

--- lib/budget.rb
+++ lib/budget.rb
@@ -1,6 +1,7 @@
 class Budget < ActiveRecord::Base
   has_many(:transactions)
   belongs_to(:user)
+  validates(:name, :amount, :user_id, :current_amount, :presence => true)
 
   def do_math
     self.current_amount += self.transactions[-1].amount
--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe(Budget) do
-  # it('adds a transaction to a budget') do
-  #   test_user = User.create({:name => 'Jim Padilla'})
-  #   test_budget = Budget.create({:name => 'Gas', :amount => 100.00, :current_amount => 100.00, :user_id => test_user.id})
-  #   test_transaction = Transaction.create({:amount => -20, :date => '12-19-2016 03:30:00', :place => 'Portland', :category => 'entertainment', :budget_id => test_budget.id})
-  #   expect(test_budget.transactions()).to(eq([test_transaction]))
-  # end
+  it('adds a transaction to a budget') do
+    test_user = User.create({:name => 'Jim Padilla'})
+    test_budget = Budget.create({:name => 'Gas', :amount => 100.00, :current_amount => 100.00, :user_id => test_user.id})
+    test_transaction = Transaction.create({:amount => -20, :date => '12-19-2016 03:30:00', :place => 'Portland', :category => 'entertainment', :budget_id => test_budget.id})
+    expect(test_budget.transactions()).to(eq([test_transaction]))
+  end
 
   describe('#do_math') do
     it('add transactions') do
@@ -19,5 +19,10 @@ describe(Budget) do
       expect(test_budget.current_amount).to(eq(80))
     end
   end
+
+  it {should validate_presence_of(:name)}
+  it {should validate_presence_of(:amount)}
+  it {should validate_presence_of(:current_amount)}
+  it {should have_many(:transactions)}
 
 end
