176b65a7420586b965d3e7c8ef4c06058321b3ce
Jonathan Thom and Tracie Weitzman <tracie@>
2016-12-19 09:52:35 -0800

    add validations for account class

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,4 +2,7 @@ require 'spec_helper'
 
 describe Account do
   it { should belong_to(:user)}
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:balance)}
+  it { should validate_uniqueness_of(:name)}
 end
