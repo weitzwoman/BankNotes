b6d0955d24f17a4c135a1979f5d16241aeaea6cc
Cody Brubaker and Jim Padilla <cody@>
2016-12-20 11:33:56 -0800

    add transaction search method and passing spec

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,4 +2,16 @@ class Transaction < ActiveRecord::Base
   belongs_to(:account)
   belongs_to(:budget)
   belongs_to(:user)
+
+  define_singleton_method(:between) do |start_date, end_date, user_id|
+     all_transactions = Transaction.where(user_id: user_id)
+     transaction_in_range = []
+     all_transactions.each do |transaction|
+       if transaction.date >= start_date and transaction.date <= end_date
+         transaction_in_range.push(transaction)
+       end
+     end
+     transaction_in_range
+   end
+
 end
