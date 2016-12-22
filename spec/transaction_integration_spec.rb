daec45b12bd656747cff229bf3bc3a3f9a719362
Jonathan Thom and Tracie Weitzman <tracie@>
2016-12-22 10:23:02 -0800

    add transaction integration spec for searching transaction history

--- spec/transaction_integration_spec.rb
+++ spec/transaction_integration_spec.rb
@@ -26,4 +26,21 @@ describe('the transactions path', {:type => :feature}) do
     click_button('delete_forever')
     expect(page).to_not have_content('Alcohol')
   end
+
+  it('will search a user\'s transaction history by date') do
+    user = User.create({:name => 'George Washington', :password => 'secure'})
+    visit('/')
+    fill_in('username', :with => 'George Washington')
+    fill_in('password', :with => 'secure')
+    click_button('Sign In')
+    account = Account.create({:name => 'Checking', :balance => 4000, :user_id => user.id})
+    budget = Budget.create({:name => 'Food', :amount => 500, :user_id => user.id, :current_amount => 500, :type_of_budget => 'Monthly' })
+    transaction1 = Transaction.create({:category => 'Groceries', :amount => 100, :account_id => account.id, :date => '12-12-2016', :place => 'Winco', :budget_id => budget.id, :user_id => user.id})
+    transaction2 = Transaction.create({:category => 'Groceries', :amount => 150, :account_id => account.id, :date => '12-11-2016', :place => 'Winco', :budget_id => budget.id, :user_id => user.id})
+    visit('/transactions')
+    fill_in('start_date', :with => '12-12-2016')
+    fill_in('end_date', :with => '12-12-2016')
+    click_button('Search')
+    expect(page).to_not have_content(150)
+  end
 end
